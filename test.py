5b5b15af20f55596f15c14bdf97780dd65a51099
Artur <magisterbrownie@gmail.com>
2022-12-07 16:50:22 +0000

    wds in every process

--- test.py
+++ test.py
@@ -3,17 +3,25 @@ import torch.nn as nn
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_multiprocessing as xmp
 import numpy as np
+from biopack.copy_env import copy_process
+import time
+from torchdata.datapipes.iter import IterableWrapper
+copy_process(59)
 
 train = np.load('data/tr_links.npy',allow_pickle=True)
 print(train)
 
 arsp = np.array_split(train,8)
-def mp_fn(index):
+def mp_fn(index, splits):
     xm.rendezvous('init')
     print(f'{index} sst')
-    #trr = splits[xm.get_ordinal()]
-    #zpp = IterableWrapper(trr[:,0]).open_files_by_fsspec(mode="rb", anon=True).webdataset()
+    trr = splits[xm.get_ordinal()]
+    zpp = IterableWrapper(trr[:,0]).open_files_by_fsspec(mode="rb", anon=True).webdataset()
+    zpi = iter(zpp)
+    print(zpi)
+    print(next(zpi))
+    time.sleep(1)
     #print(next(iter(zpp)))
     # if xm.is_master_ordinal():
     #   print(next(iter(zpp
-xmp.spawn(mp_fn,  nprocs=8, start_method='fork')
+xmp.spawn(mp_fn,  args=(arsp,), nprocs=8, start_method='fork')
