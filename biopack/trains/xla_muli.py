7983c9baf06946a44aeaca177dfbe9d9dae086fd
Artur <magisterbrownie@gmail.com>
2022-12-08 17:41:03 +0000

    Kinda training loop

--- biopack/trains/xla_muli.py
+++ biopack/trains/xla_muli.py
@@ -11,6 +11,7 @@ import torch_xla.utils.serialization as xser
 
 import numpy as np
 import time
+from tqdm import tqdm
 
 class XLAMultiTrainer:
     def __init__(self, save_pth, train_sus, test_sus):
@@ -21,7 +22,7 @@ class XLAMultiTrainer:
         self.test_sus = test_sus
         self.inp_proc = InputS1Loader()
 
-    def train(self):
+    def train(self, hyper_params=dict()):
         model = smp.Unet(
                     encoder_name="resnet34",        # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
                     encoder_weights=None,     # use `imagenet` pre-trained weights for encoder initialization
@@ -29,13 +30,13 @@ class XLAMultiTrainer:
                     classes=1,                      # model output channels (number of classes in your dataset)
                                 )
         self.model=xmp.MpModelWrapper(model)
-        xmp.spawn(self.mp_fn, args=(self.trains,),  nprocs=8, start_method='fork')
+        xmp.spawn(self.mp_fn, args=(self.trains, hyper_params),  nprocs=8, start_method='fork')
 
-    def mp_fn(self, index, splits):
+    def mp_fn(self, index, splits, hyper_params):
         torch.manual_seed(self.flags['seed'])
         device = xm.xla_device()
         inside_model = self.model.to(device)
-        self.train_loop(inside_model, splits[xm.get_ordinal()])
+        self.train_loop(inside_model, splits[xm.get_ordinal()], **hyper_params)
         xm.rendezvous('done')
         time.sleep(1)
 
@@ -44,15 +45,22 @@ class XLAMultiTrainer:
             device = torch.device('cpu')
             inside_model = inside_model.to(device)
             torch.save(inside_model.state_dict(), self.save_pth)
-    def train_loop(self,inside_model, split, epochs=1):
+
+    def train_loop(self,inside_model, 
+            split, 
+            epochs=1,
+            batch_size=3):
         inside_model.train()
         device = xm.xla_device()
         train_ds = self.get_train_dataset(split)
-        train_dl = DataLoader(train_ds, batch_size=3)
+        train_dl = DataLoader(train_ds, batch_size=3, drop_last=True)
         loss_module = nn.MSELoss(reduction='mean')
         optimizer = torch.optim.Adam(inside_model.parameters(), lr=0.02)
+        sz=split.shape[0]
         for ep in range(epochs):
             ct=0
+            if xm.is_master_ordinal():
+                prog = tqdm(total=sz)
             for i, batch in enumerate(train_dl):
                 inputs, lables = batch
                 inputs = inputs.to(dtype=torch.float32,device=device)
@@ -65,13 +73,28 @@ class XLAMultiTrainer:
                 xm.optimizer_step(optimizer)
                 xm.mark_step()
                 if(xm.is_master_ordinal()):
-                    print(f'CT {ct}')
-                    print(loss)
+                    prog.update(inputs.shape[0])
                 if(ct>=4):
                     break
                 ct+=1 
+            if(xm.is_master_ordinal()):
+                prog.close()
+                print(f'Epoch {ep} finished')
         pass
+    def validation_loop(self, model):
+        with torch.no_grad():
+            loss_module = nn.MSELoss(reduction='mean')
+            device = xm.xla_device()
+            model.eval()
+            test_ds = self.get_train_dataset(test_sus)
+            test_dl = DataLoader(test_ds, batch_size=8, drop_last=False)
+            for i, batch in enumerate(test_dl):
+                inputs, lables = batch
+                inputs = inputs.to(dtype=torch.float32,device=device)
+                lables = lables.to(dtype=torch.float32,device=device)
 
+
+            pass
 
     def get_train_dataset(self, subs):
         return get_zds_from_sus(subs, self.inp_proc)
