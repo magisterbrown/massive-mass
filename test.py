1a7582f8a58f58b208749eeb7c6982b8e62a50cf
Artur <magisterbrownie@gmail.com>
2022-12-11 01:19:18 +0000

    validation good speed

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import torch
 import os
-os.environ['XRT_DEVICE_MAP'] ="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0" 
-os.environ['XRT_WORKERS'] = "localservice:0;grpc://localhost:40934"
+#os.environ['XRT_DEVICE_MAP'] ="CPU:0;/job:localservice/replica:0/task:0/device:XLA_CPU:0" 
+#os.environ['XRT_WORKERS'] = "localservice:0;grpc://localhost:40934"
 import torch.nn as nn
 import torch_xla.core.xla_model as xm
 import torch_xla.distributed.xla_multiprocessing as xmp
@@ -9,12 +9,12 @@ import numpy as np
 from biopack.copy_env import copy_process
 import time
 from torchdata.datapipes.iter import IterableWrapper
-#copy_process(73)
+copy_process(75)
 
 train = np.load('data/tr_links.npy',allow_pickle=True)
 test = np.load('data/ts_links.npy',allow_pickle=True)
 from biopack.trains.xla_muli import XLAMultiTrainer
-trr = XLAMultiTrainer('data/res.pth',train, test, 1)
+trr = XLAMultiTrainer('data/res.pth',train, test, 8)
 print(trr)
 trr.train()
 
